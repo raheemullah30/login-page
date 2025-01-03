a0299fc1e6458c2ba4c9e354f1584e152e23322a
Raheemullah <raheemullah30@example.com>
2025-01-03 16:07:56 +0500

    Updated code

--- src/components/loginSignup.jsx
+++ src/components/loginSignup.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react'
-function loginSignuo() {
-  const [islogin , Setlogin] = useState(true)
+function loginSignup() {
+  const [islogin , Setlogin] = useState(true);
 
   return (
-    
 <div className='container'>
 
 <div className="form-container">
 <div className='form-toggle'>
-  <button className={islogin ? 'active' : ""} onClick={() => Setlogin (true)} >
+  <button className={islogin ? 'active' : ''} onClick={() => Setlogin (true)} >
     Login
   </button>
-  <button className={!islogin ? 'active' : ""} onClick={() => Setlogin (true)} >
+  <button className={!islogin ? 'active' : ''} onClick={() => Setlogin (false)} >
     Sign up
   </button>
 </div>
+{
+  islogin ? <>
+  
+  <div className='form'>
+    <h2>Login</h2>
+ <input type="email" placeholder='Email' />
+ <input type="text" placeholder='Password' />
+ <a href="#"> Forgot password?</a>
+ <button> Login</button>
+ <p> Not a member?<a href="#" onClick={()=> Setlogin (false)}>Signup now</a>
+ </p>
+    </div></> : ""
+}
 </div>
 </div>
 
@@ -22,4 +34,4 @@ function loginSignuo() {
   )
 }
 
-export default loginSignuo
+export default loginSignup
