44bfba32f8ca5adb09d507b7d898f6276c4fb894
Raheemullah <raheemullah30@example.com>
2025-01-03 16:27:28 +0500

    update commit

--- src/components/loginSignup.jsx
+++ src/components/loginSignup.jsx
@@ -20,13 +20,23 @@ function loginSignup() {
   <div className='form'>
     <h2>Login</h2>
  <input type="email" placeholder='Email' />
- <input type="text" placeholder='Password' />
+ <input type="Password" placeholder='Password' />
  <a href="#"> Forgot password?</a>
  <button> Login</button>
  <p> Not a member?<a href="#" onClick={()=> Setlogin (false)}>Signup now</a>
  </p>
-    </div></> : ""
-}
+    </div></> : 
+    <>
+    <div className='form'>
+    <h2>SignUp Form</h2>
+ <input type="email" placeholder='Email' />
+ <input type="Password" placeholder='Password' />
+ <input type="Password" placeholder='Conform Password' />
+
+ <button> Sign Up Form</button>
+
+    </div>
+    </>}
 </div>
 </div>
 
